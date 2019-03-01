e8089a2941e9853183ffb26070aaf46ea5d27a07
super-lin0 <3924578651@qq.com>
2019-03-01 21:19:34 +0800

    feat(路由) 完善路由跳转

--- src/pages/users/$id.js
+++ src/pages/users/$id.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import Link from 'umi/link';
 
 class Users extends Component {
     render() {
         const id = this.props.match.params.id;
         return (
-            <div>{id}</div>
+            <div>
+                {id}
+                <br />
+                <Link to='/users'>返回</Link>
+            </div>
         )
     }
 }
--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,12 +1,20 @@
 
 import styles from './users.css';
 import router from 'umi/router';
+import Link from 'umi/link';
 
 export default function() {
   return (
     <div className={styles.normal}>
       <h1>Page users</h1>
-      <button onClick={() => router.goBack()}>首页</button>
+      {/* <button onClick={() => router.goBack()}>首页</button> */}
+      <button onClick={() => router.push('/')}>首页</button>
+      <li>
+        <Link to='/users/张三'>张三</Link>
+      </li>
+      <li>
+        <Link to='/users/李四'>李四</Link>
+      </li>
     </div>
   );
 }
